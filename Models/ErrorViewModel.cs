0269c931cda149c15f4bbe069547998fc7506fb3
faaiz1234 <faaizalishahsyed@gmail.com>
2023-04-29 22:26:13 +0500

    added basic code

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace New_folder__3_.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
